cbb906d128c4fa7b9b4b4a3317ea21d866bb2eee
thalieskoda <thalieskoda@hotmail.com>
2023-09-11 21:37:40 -0400

    working on the product page

--- woodworkweb/src/components/products/products.jsx
+++ woodworkweb/src/components/products/products.jsx
@@ -1,35 +1,48 @@
 import styled from "styled-components";
-import { slidesData } from "../../utils/images/sliderImages";
+import { allProducts } from "../../utils/images/sliderImages";
+import { NavLink } from "react-router-dom";
 
 export const Products = () => {
   return (
-    <Container>
-      <h1>products</h1>
-      {slidesData.map((product, index) => {
-        return (
-          <SoloProduct key={index}>
-            {product.title}
-            {product.url && <ProductImage src={product.url} alt={product.title} />}
-          </SoloProduct>
-        );
-      })}
-    </Container>
+    <>
+      <Title>products</Title>
+      <Container>
+        {allProducts.map((product, index) => {
+          return (
+            <SoloProduct key={index}>
+              {product.title}
+              {product.url && (
+                <ProductImage src={product.url} alt={product.title} />
+              )}
+            </SoloProduct>
+          );
+        })}
+      </Container>
+    </>
   );
 };
 
-const SoloProduct = styled.div`
+const Title = styled.h1`
+  margin-top: 10vh;
+  margin-bottom: 10vh;
+`;
+const SoloProduct = styled(NavLink)`
   display: flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  border:1px black solid;
+  flex-direction: column;
+  align-items: center;
+  transition: transform 0.2s; 
 
-`
+  &:hover {
+    transform: scale(1.1); 
+  }
+`;
 const ProductImage = styled.img`
-  height: 100px;
-  width: 100px;
+  height: 200px;
+  width: 200px;
 `;
 const Container = styled.div`
   height: 100vh;
-  
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: auto auto auto;
 `;
--- woodworkweb/src/utils/images/sliderImages.js
+++ woodworkweb/src/utils/images/sliderImages.js
@@ -7,3 +7,18 @@ export const slidesData = [
   { title: "Beer", url: Image1 },
   { title: "Father's Day", url: Image2 },
 ];
+
+export const allProducts = [
+  { title: "Our classic custom", url: Image },
+  { title: "Beer", url: Image1 },
+  { title: "Father's Day", url: Image2 },
+  { title: "product3", url: Image },
+  { title: "Product4", url: Image1 },
+  { title: "Product5", url: Image2 },
+  { title: "Product6", url: Image },
+  { title: "Product7", url: Image1 },
+  { title: "Product8", url: Image2 },
+  { title: "Product9", url: Image },
+  { title: "Product10", url: Image1 },
+  { title: "Product11", url: Image2 },
+]
